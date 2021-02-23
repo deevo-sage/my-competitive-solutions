1d4591a7eb35b6a5f428368a8926c70ea345c9a9
deevo-sage <sidharthsahniii@gmail.com>
2021-02-23 19:39:09 +0530

    added javascript dates 10 days of javascriptr hackerrank

---
+++ javascript/10 days of javascript/javascript dates/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    // Write your code here
+    var date = new Date(dateString)
+    var day = date.getDay()
+  for(let i=0;i<7;i++)
+  {
+   if(day==i)
+   {dayName=days[i]}
+      
+  }
+  
+    return dayName;
+}
+
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
